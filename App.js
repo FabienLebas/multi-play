ab69db759dd1c20cecf55149e77c3956bf83ee8b
FABIEN <fabien.lebas@decathlon.com>
2021-11-05 17:12:50 +0300

    add navigation

--- App.js
+++ App.js
@@ -1,37 +1,29 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View } from 'react-native';
 import {Button} from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack' ;
+
+import HomeScreen from './components/home';
 import Chifoumi from './components/chifoumi';
-import NumberOfPlayers from './components/numberOfPlayers'
+
+const Stack = createNativeStackNavigator();
 
 export default class App extends Component {
   render(){
     return (
-      <View style={styles.container}>
-        <NumberOfPlayers/>
-        <Text style={styles.intro}>Créé par Tom !</Text>
-        <Text style={styles.intro}>Notre tout premier jeu :</Text>
-        <Text style={styles.gameTitle}>CHIFOUMI</Text>
-        <Chifoumi/>
-      </View>
-
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Lobby"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name="Chifoumi"
+            component={Chifoumi}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  intro: {
-    fontSize: 20
-  },
-  gameTitle: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-    padding: 20
-  }
-});
---
+++ components/home.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View } from 'react-native';
+import {Button} from 'react-native-elements';
+
+const HomeScreen = ({ navigation }) => {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.intro}>Créé par Tom !</Text>
+      <Text style={styles.intro}>Notre tout premier jeu :</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate('Chifoumi', { name: 'Jane' })
+        }}
+        title='CHIFOUMI'
+        type='outline'
+        style={styles.game}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  intro: {
+    fontSize: 20
+  },
+  game: {
+    padding: 20
+  }
+});
+
+export default HomeScreen;
